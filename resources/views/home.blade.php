11dd5a9295ac5ba09f3c54dd8502c90fff469078
NiteshHamal <nitesh0hamal@gmail.com>
2024-06-10 16:48:45 +0545

    mero flight logo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -178,7 +178,7 @@
                         <img src="{{url('assets/img/Mahindra-Logo.webp')}}" alt="Mahindra Logo">
                     </div>
                     <div class="client-logo-item">
-                        <img src="{{url('assets/img/Mero-Flight-logo.webp')}}" alt="Mero Flight Logo">
+                        <img src="{{url('assets/img/Mero-Flight-Logo.webp')}}" alt="Mero Flight Logo">
                     </div>
                     <div class="client-logo-item">
                         <img src="{{url('assets/img/Vector_Smart_Object1-01-1-1.webp')}}" alt="Vector Smart Object Logo">
