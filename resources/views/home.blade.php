6f176dbad6a6fff814dfbc394b00f6c5db5e758f
NiteshHamal <nitesh0hamal@gmail.com>
2024-07-23 10:52:16 +0545

    change button to explore all

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -299,7 +299,7 @@
                         <div class="nav nav-tabs" id="nav-tab" role="tablist">
                             <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Yearly</button>
                             <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Monthly</button>
-                            <a href={{url('service')}}><button class="nav-link" id="nav-profile-tab" type="button">More</button></a>
+                            <a href={{url('service')}}><button class="nav-link" id="nav-profile-tab" type="button">Explore All</button></a>
                         </div>
                     </nav>
                     <div class="tab-content" id="nav-tabContent">
